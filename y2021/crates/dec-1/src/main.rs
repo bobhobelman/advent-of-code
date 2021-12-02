b05c00b518f538ce857ad17d133bc7934cbd396e
Bob Hobelman <bob.hobelman@nedap.com>
2021-12-02 20:34:33 +0100

    Day 1 - cleanup

--- y2021/crates/dec-1/src/main.rs
+++ y2021/crates/dec-1/src/main.rs
@@ -1,70 +1,49 @@
+use itertools::Itertools;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
 
 fn main() {
-    let times = gt("./resources/input-dec-1");
-    println!("{:?}", times);
+    if let Ok(numbers) = read_input("./resources/input-dec-1") {
+        let times = cmp_pref_next(numbers.clone());
+        println!("{:?}", times);
 
-    let times = gt_walking_average("./resources/input-dec-1");
-    println!("{:?}", times);
+        let times = cmp_abc_bcd(numbers.clone());
+        println!("{:?}", times);
+    }
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+fn cmp_pref_next(numbers: Vec<i32>) -> i32 {
+    let mut count = 0;
+    for (pref, current) in numbers.into_iter().tuple_windows() {
+        if current.gt(&pref) {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn cmp_abc_bcd(numbers: Vec<i32>) -> i32 {
+    let mut count = 0;
+    for (a, b, c, d) in numbers.into_iter().tuple_windows() {
+        let pref = a + b + c;
+        let current = b + c + d;
+        if pref < current {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn read_input<P>(filename: P) -> io::Result<Vec<i32>>
     where
         P: AsRef<Path>,
 {
     let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-fn gt(input: &str) -> Option<i32> {
-    if let Ok(lines) = read_lines(input) {
-        let mut previous: Option<i32> = None;
-        let mut count = 0;
-
-        for line in lines {
-            if let Ok(value) = line {
-                if let Ok(i) = value.parse::<i32>() {
-                    if let Some(previous) = previous {
-                        if i.gt(&previous) {
-                            count += 1;
-                        }
-                    }
-                    previous = Some(i);
-                }
-            }
-        }
-        return Some(count);
-    }
-    None
-}
-
-fn gt_walking_average(input: &str) -> Option<i32> {
-    if let Ok(lines) = read_lines(input) {
-        let mut current: (Option<i32>, Option<i32>, Option<i32>) = (None, None, None);
-        let mut previous: (Option<i32>, Option<i32>, Option<i32>) = (None, None, None);
-        let mut count = 0;
-
-        for line in lines {
-            if let Ok(value) = line {
-                if let Ok(i) = value.parse::<i32>() {
-                    current = (Some(i), previous.0, previous.1);
-                    if let (Some(x), Some(y), Some(z)) = previous {
-                        let previous = x + y + z;
-                        let next = current.0.unwrap() + current.1.unwrap() + current.2.unwrap();
-                        // println!("p: {} {} {} c:{} {} {} - 1", z, y, x, current.0.unwrap(), current.1.unwrap(), current.2.unwrap());
-
-                        if next.gt(&previous) {
-                            count += 1;
-                        }
-                    }
-                }
-                previous = current;
-            }
-        }
-        return Some(count);
-    }
-    None
+    let x: Vec<i32> = io::BufReader::new(file)
+        .lines()
+        .map(|l| l.unwrap().parse::<i32>().unwrap())
+        .collect();
+    Ok(x)
 }
