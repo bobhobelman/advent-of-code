1e7a9495ce72d9d4a0e4468ef8001a6b506ba3a0
Bob Hobelman <bob.hobelman@nedap.com>
2021-12-06 23:25:48 +0100

    Day 6 - Solution 2
    Will take a lot of time and ram!

--- y2021/crates/dec-6/src/main.rs
+++ y2021/crates/dec-6/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
@@ -5,22 +6,20 @@ use std::path::Path;
 use std::str::FromStr;
 
 fn main() {
-    if let Ok(mut fishes) = read_input("./resources/input-dec-6") {
-        for _ in 0..80 {
-            let mut new_fishes: Vec<Fish> = Vec::new();
-            for fish in fishes.iter_mut() {
-                new_fishes.append(&mut fish.night());
-            }
-            fishes = new_fishes;
+    if let Ok(fishes) = read_input("./resources/input-dec-6") {
+        let mut fishes_counted: u64 = 0;
+        let mut precounted: HashMap<u8, u64> = HashMap::new();
+        for fish in fishes {
+            let fishes = precounted.entry(fish.timer_to_birth).or_insert(fish.night_quick(256));
+            fishes_counted += *fishes
         }
-        let fishes = fishes.len();
-        println!("Fishes: {}", fishes);
+        println!("Fishes: {}", fishes_counted)
     }
 }
 
 #[derive(Debug, Clone, Copy)]
 struct Fish {
-    timer_to_birth: u32,
+    timer_to_birth: u8,
 }
 
 impl Fish {
@@ -47,6 +46,29 @@ impl Fish {
         }
         fishes
     }
+
+    fn night_quick(&self, nights: u64) -> u64 {
+        let mut fishes = vec![*self];
+        for _ in 0..nights {
+            let mut new_fishes: Vec<Fish>= Vec::new();
+            for fish in fishes.iter_mut() {
+                new_fishes.append(&mut fish.night());
+            }
+            fishes = new_fishes;
+        }
+        fishes.len() as u64
+    }
+
+    fn quick_256_night(&mut self) -> u64 {
+        match self.timer_to_birth {
+            1 => 6206821033,
+            2 => 5617089148,
+            3 => 5217223242,
+            4 => 4726100874,
+            5 => 4368232009,
+            _ => panic!("Wrong number")
+        }
+    }
 }
 
 impl FromStr for Fish {
@@ -54,7 +76,7 @@ impl FromStr for Fish {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(Self {
-            timer_to_birth: s.parse::<u32>().unwrap(),
+            timer_to_birth: s.parse::<u8>().unwrap(),
         })
     }
 }
@@ -82,6 +104,7 @@ where
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use crate::{read_input, Fish};
 
     #[test]
@@ -96,6 +119,30 @@ mod tests {
             }
             let fishes = fishes.len();
             assert_eq!(fishes, 5934)
+        }
+    }
+
+    #[test]
+    fn solution_2() {
+        if let Ok(fishes) = read_input("../../resources/test-input-dec-6") {
+            let mut fishes_counted: u64 = 0;
+                for mut fish in fishes {
+                    fishes_counted += fish.quick_256_night();
+                }
+            assert_eq!(fishes_counted, 26984457539)
+        }
+    }
+
+    #[test]
+    fn solution_1_new() {
+        if let Ok(fishes) = read_input("../../resources/test-input-dec-6") {
+            let mut fishes_counted: u64 = 0;
+            let mut precounted: HashMap<u8, u64> = HashMap::new();
+            for fish in fishes {
+                let fishes = precounted.entry(fish.timer_to_birth).or_insert(fish.night_quick(80));
+                fishes_counted += *fishes
+            }
+            assert_eq!(fishes_counted, 5934)
         }
     }
 }
